6fa10ba12412d69dcea9cea54f7c2bb9ba3dbd0b
Chris <czakian@gmail.com>
2011-09-16 19:58:45 -0400

    working parser with state machine

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -2,38 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "safe_funcs.h"
+#include <ctype.h>
 #define TRUE 1
 #define FALSE 0
 #define SPACE 32
-#define REGISTRY_SIZE 6
-
-char** init_registry() {
-	char **registry; 
-	registry = (char **) safe_malloc(REGISTRY_SIZE * sizeof(char*), "malloc failed for registry");
-
-	for(int i = 0; i < REGISTRY_SIZE; i++) {
-		registry[i] = (char*) safe_malloc(4 * sizeof(char), "error in malloc for registry elements");
-	}
-
-	strcpy(registry[0],"<");
-	strcat(registry[0], "\0");
-
-	strcpy(registry[1]," ");
-	strcat(registry[1], "\0");
-
-	strcpy(registry[2],">");
-	strcat(registry[2], "\0");
-
-	strcpy(registry[3],"&");
-	strcat(registry[3], "\0");
-
-	strcpy(registry[4],";");
-	strcat(registry[4], "\0");
-
-	strcpy(registry[5],"|");
-	strcat(registry[5], "\0");
-	return registry;
-}
 
 typedef struct node {
 	char* datum;
@@ -41,108 +13,24 @@ typedef struct node {
 	struct node* next;
 } node;
 
-int cleanup_registry(char** registry) {
-	for(int i = 0; i < REGISTRY_SIZE; i++) {
-		free(registry[i]);
-	}
-	free(registry);
-	return 0;
-}
-
 int cleanup_nodes(node *n) {
 	if(n != NULL) {
 		free(n->datum);
 		cleanup_nodes(n->next);
 	}
 	free(n);
-	return 0;
-}
-
-//get the left most instruction
-int select_instruction(char* expr, char** registry) {
-	int min = -1;
-	int registry_loc = -1;
-	if(expr != NULL && registry != NULL) {
-		char *tmp = "\0";
-
-		for(int i =0; i < REGISTRY_SIZE; i++) {
-			tmp = strstr(expr, registry[i]);
-
-			if((tmp - expr) >= 0 && (tmp - expr) < REGISTRY_SIZE) {
-				if(min == -1) {
-					min = tmp - expr;
-					registry_loc = i;
-				}	else if((tmp - expr) < min) {
-					min = tmp - expr;
-					registry_loc = i;
-				}
-			}
-		}
-
-	} else {
-		perror("fatal error in select instructions: NULL values passed to parser");
-	}
-
-	return registry_loc;
-}
-
-node *interp(char* expr, node *prev,  node* n, char** registry) {
-	node *cmd = safe_malloc(sizeof(node), "fatal error: node could not be created");//freed in cleanup_nodes
-	int registry_loc = select_instruction(expr, registry);
-	char *instruction;
-	if(registry_loc != -1){
-		instruction = registry[registry_loc];
-		puts(instruction);
-	}
-	//freed in cleanup_nodes as datum
-	char *cpy = safe_malloc(strlen(expr)*sizeof(char)+1, "fatal error: interp could not malloc data for expression");
-	strcpy(cpy, expr);
-
-	if(registry_loc != -1 && (strtok(cpy,instruction)) != NULL) {
-		n->datum = safe_malloc(strlen(cpy)*sizeof(char)+1, "fatal error: interp could not malloc data for node");
-		strcpy(n->datum,cpy);
-
-		int cpylen = strlen(cpy);
-		char* new_expr = safe_malloc(((strlen(expr)+1)-cpylen)*sizeof(char)+1, "fatal error: interp could not malloc data for expression");
-		strcpy(new_expr,expr+cpylen+1);
-		free(expr);
-
-		n->prev = safe_malloc(sizeof(node), "fatal error: malloc could not allocate memory for node");
-		n->next = safe_malloc(sizeof(node), "fatal error: malloc could not allocate memory for node");
-		n->prev = prev;
-		n->next = cmd;
-
-		cmd->datum = safe_malloc(4*sizeof(char), "fatal error: malloc could not allocate memory for node");
-		cmd->datum = instruction;
-
-		cmd->prev = safe_malloc(sizeof(node), "fatal error: malloc could not allocate memory for node");
-		cmd->next = safe_malloc(sizeof(node), "fatal error: malloc could not allocate memory for node");
-		cmd->prev = n;
-		node *new_node = safe_malloc(sizeof(node), "fatal error: node could not be created");
-		cmd->next = interp(new_expr, cmd, new_node, registry);
-
-	} else if (expr != NULL) {
-		n->datum = expr;
-		n->next = NULL;
-		n->prev = prev;
-	} else {
-		perror("Error: no matching command found on input");
-	}
-
-	return n;
 }
 
 int main(int argc, char** argv) {
 
-	char** registry = init_registry();
 	char* bucket;
 	int read = 0;
 	int bucket_size = 256;
-	char** cmd;
+	node *head = NULL;
+	node * cur = NULL;
+
 	//initialize with prompt
 	char *prompt;
-	node *n = safe_malloc(sizeof(node), "fatal error: node could not be created");
-	node *head = n;
 	if(argc >= 2)
 		prompt = argv[1];
 	else
@@ -155,33 +43,181 @@ int main(int argc, char** argv) {
 		char c;
 		while((c = getchar()) != '\n') {
 			if(c == EOF) {
-				free(bucket);
 				cleanup_nodes(head);
-				cleanup_registry(registry);
+				free(bucket);
 				exit(0);
 			}
 
-			bucket = (char *) chksize(bucket, read, &bucket_size, bucket_size*2+1);
-			bucket[read] = c;
-			read += 1;
+			if(isalnum(c) || c == SPACE) {
+				bucket = (char *) chksize(bucket, read, &bucket_size, bucket_size*2+1);
+				bucket[read] = c;
+				read += 1;
+			} else if(c == ';') {
+
+				if(read >= 1) {
+					node *s = (node *) safe_malloc(sizeof(node), "error creating node");
+					s->datum = (char *) safe_malloc(read*sizeof(char)+1, "could not malloc datum in node");
+					strncpy(s->datum, bucket, read);
+					if(cur != NULL)
+						cur->next = s;
+					read = 0;
+					s->prev = cur;
+					s->next = NULL;
+					cur = s;
+					if(head == NULL)
+						head = cur;
+				} 
+
+				node *n = safe_malloc(sizeof(node), "error creating node");
+				if(cur != NULL)
+					cur->next = n;
+				n->datum = (char *) safe_malloc(2*sizeof(char), "could not malloc datum in node");
+				n->datum[0] = c;
+				n->datum[1] = '\0';
+				n->prev = cur;
+				n->next = NULL;
+				cur = n;
+				if(head == NULL)
+					head = cur;
+			} else if(c == '|') {
+
+				if(read >= 1) {
+					node *s = (node *) safe_malloc(sizeof(node), "error creating node");
+					s->datum = (char *) safe_malloc(read*sizeof(char)+1, "could not malloc datum in node");
+					strncpy(s->datum, bucket, read);
+					if(cur != NULL)
+						cur->next = s;
+					read = 0;
+					s->prev = cur;
+					s->next = NULL;
+					cur = s;
+					if(head == NULL)
+						head = cur;
+				} 
+
+				node *n = safe_malloc(sizeof(node), "error creating node");
+				if(cur != NULL)
+					cur->next = n;
+				n->datum = (char *) safe_malloc(2*sizeof(char), "could not malloc datum in node");
+				n->datum[0] = '|';
+				n->datum[1] = '\0';
+				n->prev = cur;
+				n->next = NULL;
+				cur = n;
+				if(head == NULL)
+					head = cur;
+			} else if(c == '&') {
+
+				if(read >= 1) {
+					node *s = (node *) safe_malloc(sizeof(node), "error creating node");
+					s->datum = (char *) safe_malloc(read*sizeof(char)+1, "could not malloc datum in node");
+					strncpy(s->datum, bucket, read);
+					if(cur != NULL)
+						cur->next = s;
+					read = 0;
+					s->prev = cur;
+					s->next = NULL;
+					cur = s;
+					if(head == NULL)
+						head = cur;
+				} 
+
+				node *n = safe_malloc(sizeof(node), "error creating node");
+				if(cur != NULL)
+					cur->next = n;
+				n->datum = (char *) safe_malloc(2*sizeof(char), "could not malloc datum in node");
+				n->datum[0] = c;
+				n->datum[1] = '\0';
+				n->prev = cur;
+				n->next = NULL;
+				cur = n;
+				if(head == NULL)
+					head = cur;
+			} else if(c == '>') {
+
+				if(read >= 1) {
+					node *s = (node *) safe_malloc(sizeof(node), "error creating node");
+					s->datum = (char *) safe_malloc(read*sizeof(char)+1, "could not malloc datum in node");
+					strncpy(s->datum, bucket, read);
+					if(cur != NULL)
+						cur->next = s;
+					read = 0;
+					s->prev = cur;
+					s->next = NULL;
+					cur = s;
+					if(head == NULL)
+						head = cur;
+				} 
+
+				node *n = safe_malloc(sizeof(node), "error creating node");
+				if(cur != NULL)
+					cur->next = n;
+				n->datum = (char *) safe_malloc(2*sizeof(char), "could not malloc datum in node");
+				n->datum[0] = c;
+				n->datum[1] = '\0';
+				n->prev = cur;
+				n->next = NULL;
+				cur = n;
+				if(head == NULL)
+					head = cur;
+			} else if(c == '<') {
+
+				if(read >= 1) {
+					node *s = (node *) safe_malloc(sizeof(node), "error creating node");
+					s->datum = (char *) safe_malloc(read*sizeof(char)+1, "could not malloc datum in node");
+					strncpy(s->datum, bucket, read);
+					if(cur != NULL)
+						cur->next = s;
+					read = 0;
+					s->prev = cur;
+					s->next = NULL;
+					cur = s;
+					if(head == NULL)
+						head = cur;
+				} 
+
+				node *n = safe_malloc(sizeof(node), "error creating node");
+				if(cur != NULL)
+					cur->next = n;
+				n->datum = (char *) safe_malloc(2*sizeof(char), "could not malloc datum in node");
+				n->datum[0] = c;
+				n->datum[1] = '\0';
+				n->prev = cur;
+				n->next = NULL;
+				cur = n;
+				if(head == NULL)
+					head = cur;
+			}
 		}
-		bucket[read] = '\0';
 
-		printf("input: %s\n", bucket);
-		n =	interp(bucket, NULL, n, registry);
-		read = 0;
+		if(read >= 1) {
+			node *s = (node *) safe_malloc(sizeof(node), "error creating node");
+			s->datum = (char *) safe_malloc(read*sizeof(char)+1, "could not malloc datum in node");
+			strncpy(s->datum, bucket, read);
+			read = 0;
+			if(cur != NULL)
+				cur->next = s;
+			s->prev = cur;
+			s->next = NULL;
+			cur = s;
+		}
 
-		node *tmp = head;
-		printf("instruction is: ");
-		while(tmp != NULL) {
-			printf("(%s)", tmp->datum);
-			tmp = tmp->next;
+		if(head == NULL)
+			head = cur;
+
+		cur=head;
+		while(cur != NULL && cur->datum != NULL) {
+			printf("%s ", cur->datum);
+			cur = cur->next; 
 		}
 		printf("\n");
+
+		cleanup_nodes(head);
+		head = NULL;
 	} //while TRUE
 
-	cleanup_registry(registry);
-	cleanup_nodes(head);
+	//cleanup_nodes(head);
+	free(bucket);
 	return 0;
 } 
 

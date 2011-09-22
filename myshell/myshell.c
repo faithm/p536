ede20daa8e8dcbeaba2a292986106ef4ccd75ca1
Chris <czakian@gmail.com>
2011-09-22 02:13:46 -0400

    yay! finished sheellll

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -38,7 +38,6 @@ typedef struct node {
 	int fd[2];
 } node;
 
-
 int cleanup_nodes(node *n) {
 	if(n != NULL) {
 		free(n->datum);
@@ -46,6 +45,7 @@ int cleanup_nodes(node *n) {
 			for(int i = 0; n->args[i] != NULL; i++) {
 				free(n->args[i]);
 			}
+			free(n->args);
 		}
 		cleanup_nodes(n->next);
 	}
@@ -61,83 +61,62 @@ node *create_node(){
 	return n;
 }
 
-void pipe_out(node *prev) {
-	if(close(prev->fd[0]) == -1) { //child doesn't read
-		perror("fatal error: closing file descriptor in child failed");
-	}
-	if(dup2(prev->fd[1], STDOUT_FILENO) == -1) { //write to stdout
-		perror("fatal error: dup2 failed in file redirection");
-	}
-	if(close(prev->fd[1]) == -1) { //close write side fd
-		perror("fatal error: closing file descriptor in child failed");
-	}
-}
+node *pipline(node *cur) {
+	int fd = -1;
+	while (cur != NULL) {
+		if(cur->datum != NULL)
+			cur = cur->next;
 
-void pipe_in(node *prev) {
-	if(close(prev->fd[1]) == -1) { //don't write
-		perror("fatal error: closing file descriptor in parent failed");
-	}
-	if(dup2(prev->fd[0], STDIN_FILENO) == -1) { //read from stdin
-		perror("fatal error: dup2 failed in file redirection");
-	}
-	if(close(prev->fd[0]) == -1) { //close read side fd
-		perror("fatal error: closing file descriptor in parent failed");
-	}
-}
+		cur->fd[0] = -1;
+		cur->fd[1] = -1;
 
-void pipline(node *cur, int fd) {
-	if(cur->datum != NULL)
-		cur = cur->next;
+		if(cur->next != NULL && cur->next->datum != NULL && cur->next->datum[0] == '|')
+			if(pipe(cur->fd) == -1)
+				perror("fatal error: pipe failed");
 
-	cur->fd[0] = -1;
-	cur->fd[1] = -1;
+		int pid;
+		if(((pid = fork()) == -1))
+			perror("fork failed in creating pipeline");
 
-	if(cur->next != NULL)
-		if(pipe(cur->fd) == -1)
-			perror("fatal error: pipe failed");
+		if(pid == 0) { //child
 
-	int pid;
-	if((pid = fork() == -1))
-		perror("fork failed in creating pipeline");
+			if(fd != -1) {
+				if(dup2(fd, STDIN_FILENO) == -1)
+					perror("error in duping std in");
 
-	if(!pid) { //child
-		if(fd != -1) {
-			if(dup2(fd, STDIN_FILENO) == -1)
-				perror("error in duping std in");
+				if(close(fd) == -1)
+					perror("error closing passed file descriptor");
+			}
 
-			if(close(fd) == -1)
-				perror("error closing passed file descriptor");
-		}
+			if(cur->fd[1] != -1) {
+				if(dup2(cur->fd[1], STDOUT_FILENO) == -1)
+					perror("error in duping std out");
 
-		if(cur->fd[1] != -1) {
-			if(dup2(cur->fd[1], STDOUT_FILENO) == -1)
-				perror("error in duping std out");
-
-			if(close(cur->fd[1]) == -1)
+				if(close(cur->fd[1]) == -1)
 					perror("error closing file descriptor");
+			}
+
+			if(cur->fd[0] != -1) {
+				if(close(cur->fd[0]) == -1)
+					perror("error closing file descriptor");
+			}
+
+			execvp(cur->args[0], cur->args);
+			perror("");
+		} else { //parent
+			int status;
+			waitpid(-1, &status, 0);
+
+			if(fd != -1) {
+				close(fd);
+			}
+
+			if(cur->fd[1] != -1) {
+				close(cur->fd[1]);
+			}
 		}
-
-		if(cur->fd[0] != -1) {
-			if(close(cur->fd[0]) == -1)
-				perror("error closing file descriptor");
-		}
-
-		execvp(cur->args[0], cur->args);
-		perror("");
-	} else { //parent
-		if(fd != -1) {
-			close(fd);
-		}
-
-		if(cur->fd[1] != -1) {
-			close(cur->fd[1]);
-		}
-
-		if(cur->next != NULL) 
-			pipline(cur->next, cur->fd[0]);
-
-		int status;
-		waitpid(-1, &status, 0);
+		fd = cur->fd[0];
+		cur = cur->next;
 	}
 }
 
@@ -170,11 +149,12 @@ int main(int argc, char** argv) {
 
 			if(c == EOF) {
 				cleanup_nodes(head);
+				free(cur);
 				free(bucket);
 				exit(0);
 			} 
 
-			if(isalnum(c) || c >= 33 && c <= 47 || c >= 91 && c <= 96 || c == 123 || c == 125 || c == 126) {
+			if(isalnum(c) || c >= 33 && c <=37 || c >=39 && c <= 47 || c >= 91 && c <= 96 || c == 123 || c == 125 || c == 126) {
 				bucket = (char *) chksize(bucket, bucket_read, &bucket_size, bucket_size*2+1);
 				bucket[bucket_read] = c;
 				bucket_read += 1;
@@ -183,10 +163,10 @@ int main(int argc, char** argv) {
 				if(bucket_read >= 1) {
 					if(cur->args == NULL) 
 						cur->args = safe_malloc(args_size*sizeof(char *), "fatal error: could not malloc args array");
-					cur->args[arg_c] = (char *) safe_malloc(bucket_read*sizeof(char)+1, "could not malloc datum in node");
+					cur->args[arg_c] = (char *) safe_malloc(bucket_read*sizeof(char)+2, "could not malloc datum in node");
 					strncpy(cur->args[arg_c], bucket, bucket_read);
 					cur->args[arg_c+1] = '\0';
-					cur->args[arg_c][bucket_read+1] = '\0';
+					cur->args[arg_c][bucket_read] = '\0';
 					bucket_read = 0;
 					arg_c += 1;
 				}
@@ -231,37 +211,37 @@ int main(int argc, char** argv) {
 		cur=head;
 		if(head != NULL && head->args != NULL && strstr(head->args[0], "exit")) {
 			cleanup_nodes(head);
+			free(cur);
 			free(bucket);
 			exit(0);
 		}
 
-		/*
-		while(cur != NULL) {
-			int a = 0;
-			while(cur->args != NULL && cur->args[a] != NULL) {
-				printf("arg %d is: %s\n",a,cur->args[a]);
-				a++;
-			}
-			cur = cur->next;
-		}
-		*/
 		cur = head;
-
 		//arguments section
 
 		int pid, status;
-		int AMPERSAND = 0;
 		while(cur != NULL) {
+		int AMPERSAND = 0;
 
 			//start command states:
 
-			if(cur != NULL && cur->datum != NULL && memchr(cmds,cur->datum[0], 1)) {
-				AMPERSAND = 1; //WNOHANG
-			}
+		if(cur->datum != NULL && cur->datum[0] == '&'){
+			AMPERSAND = WNOHANG;
+			node *tmp = cur->prev;
 
-			if(cur != NULL && cur->datum != NULL && cur->datum[0] == ';') { //skip over this command since we don't want to execute it
-				cur = cur->next;
+			if(cur->next != NULL && cur->next->datum != NULL || cur->next->args != NULL){
+				tmp->next = cur->next;
+			cur->next->prev = tmp;
+			} else {
+				tmp->next = NULL;
+				free(cur->next);
 			}
+			free(cur->datum);
+			free(cur->args);
+			free(cur);
+			cur = tmp;
+		}
+
 
 			if(cur->args != NULL && cur->args[0] != NULL && strstr(cur->args[0], "cd")) { //cd as a builtin command
 
@@ -270,7 +250,7 @@ int main(int argc, char** argv) {
 				}
 
 			} else if(cur != NULL && cur->datum != NULL && cur->datum[0] == '|') { //pipe
-				pipline(cur->prev, -1);
+				cur = pipline(cur->prev);
 
 			} else if (cur != NULL  && cur->datum != NULL && cur->datum[0] == '>') { //redirect output
 
@@ -278,28 +258,33 @@ int main(int argc, char** argv) {
 					perror("could not fork in output redirecton");
 
 				if(pid == 0) { //child
+					char **args = cur->prev->args;
 
-					char* arg = cur->prev->datum;
-					int fd;
-					if(cur->next->datum[0] == '>') { //handle >> command
-						cur = cur->next; //advance the args list past the second >
-						if((fd = open(cur->next->datum, O_RDWR | O_CREAT | O_APPEND, 0666)) < 0)
+					if(cur->next->datum == NULL && cur->next->args == NULL) { //handle >> command
+						cur = cur->next->next->next; //advance the args list past the second >
+						cur->fd[1] = -1;
+						cur->fd[0] = -1;
+						if((cur->fd[1] = open(cur->args[0], O_RDWR | O_CREAT | O_APPEND, 0666)) < 0)
 							perror("could not open fd in input redirection");
 
 					} else {
-						if((fd = open(cur->next->datum, O_RDWR | O_CREAT, 0666)) < 0)
+						cur = cur->next;
+						cur->fd[1] = -1;
+						cur->fd[0] = -1;
+						if((cur->fd[1] = open(cur->args[0], O_RDWR | O_CREAT, 0666)) < 0)
 							perror("could not open fd in input redirection");
 					}
-					if(dup2(fd, STDOUT_FILENO) < 0) {
+
+					if(dup2(cur->fd[1], STDOUT_FILENO) < 0) {
 						perror("fatal error: dup2 failed in file redirection");
 					}
-					if(close(fd) < 0) {
+					if(close(cur->fd[1]) < 0) {
 						perror("close operation in redirection failed");
 					}
-					execvp(cur->args[0], cur->args);
+					execvp(args[0], args);
 					perror("");
 				} else {
-					if(cur != NULL && cur->datum != NULL && cur->next->datum[0] == '>') 
+					if(cur != NULL && cur->next->datum != NULL && cur->next->datum[0] == '>') 
 						cur = cur->next; //advance the args list past the second >
 					cur = cur->next;
 					waitpid(-1, &status, AMPERSAND);
@@ -311,18 +296,19 @@ int main(int argc, char** argv) {
 					perror("could not fork in file redirecton");
 
 				if(pid == 0) { //child
+					cur->next->fd[1] = -1;
+					cur->next->fd[0] = -1;
 					char* arg = cur->prev->datum;
-					int fd;
-					if((fd = open(cur->next->datum, O_RDONLY)) < 0){
+					if((cur->next->fd[0] = open(cur->next->args[0], O_RDONLY)) < 0){
 						perror("could not open fd in input redirection");
 					} else {
-						if(dup2(fd, STDIN_FILENO) == -1) {
+						if(dup2(cur->next->fd[0], STDIN_FILENO) == -1) {
 							perror("fatal error: dup2 failed in file redirection");
 						}
-						if(close(fd) < 0) {
+						if(close(cur->next->fd[0]) < 0) {
 							perror("close operation in redirection failed");
 						}
-						execvp(cur->args[0], cur->args);
+						execvp(cur->prev->args[0], cur->prev->args);
 						perror("");
 					}
 
@@ -330,14 +316,22 @@ int main(int argc, char** argv) {
 					cur = cur->next;
 					waitpid(-1, &status, AMPERSAND);
 				}
-			} else {
+			} else if (AMPERSAND || cur->next == NULL || cur->datum != NULL && cur->datum[0] ==';') { 
 				if((pid = fork()) == -1) 
 					perror("could not fork in run command");
 
 				if(pid == 0) { //child
+
+					if(cur->datum != NULL && cur->datum[0] == ';')
+						cur = cur->prev;
+
 					execvp(cur->args[0], cur->args);
 					perror("");
 				} else {
+					if(AMPERSAND) {
+						if(cur->next != NULL && cur->next->datum != NULL && cur->next->datum[0] == ';')
+							cur = cur->next;
+					}
 					waitpid(-1, &status, AMPERSAND);
 				}
 			}
